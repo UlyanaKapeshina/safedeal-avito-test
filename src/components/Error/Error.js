318b97566d695ac1f45351974221b4d92a26d232
Ulyana <ulyanakapeshina@gmail.com>
2020-01-09 15:10:42 +0300

    добавлены стили

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -4,7 +4,9 @@ import "./Error.css";
 const Error = props => {
   return (
     <div className={props.className}>
-      <div>Ошибка: {props.message}</div>
+      <div className="error">
+        <p className="error_text">Ошибка: {props.message}</p>
+      </div>
     </div>
   );
 };
